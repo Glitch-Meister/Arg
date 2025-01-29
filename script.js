90c9b450f7a50aa21b8783fb6c318057a9f5dd5a
Glitch-Meister <112450425+Glitch-Meister@users.noreply.github.com>
2025-01-29 14:33:41 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let username;
+let password;
+
 document.getElementById("mySubmit").onclick = function() {
-  
-  username = document.getElementById("myText").value;
+  username = document.getElementById("username_input").value;
   username = username.toLowerCase();
+
+  password = document.getElementById("password_input").value;
   
-  if (username == "glitchmeister b-45"){
+  if (username == "glitchmeister b-45" && password == "YourMom69"){
     document.getElementById("myh1").style.color = "red";
     document.getElementById("myh1").textContent = "Y29tbWFuZC5wcm9tdC9yZXNldA==";
+  } else if (username == "" && password == ""){
+    document.getElementById("myh1").textContent = `Please enter a username and password.`;
   } else if (username == ""){
+    document.getElementById("myh1").textContent = `Please enter a username.`;
+  } else if (password == ""){
     document.getElementById("myh1").textContent = `Please enter a password.`;
   }
   else {
-    document.getElementById("myh1").textContent = `Invalid password.`;
+    document.getElementById("myh1").textContent = `Invalid username or password.`;
   }
 }
